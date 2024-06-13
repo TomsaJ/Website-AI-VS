14aef49ef47619c15f23086c742922e9de1d8893
juliantomsa <julian.tomsa@gmail.com>
2024-06-13 12:13:36 +0200

    chnge DB class

--- src/db.py
+++ src/db.py
@@ -1,30 +1,29 @@
+import json
 import mysql.connector
 from mysql.connector import Error
-import json
 
 class DB:
+    def __init__(self):
+        self.config = self.load_db_config()
 
+    @staticmethod
     def load_db_config():
-        with open('db_config.json', 'r') as file:
+        with open('src/db_config.json', 'r') as file:
             config = json.load(file)
         return config
 
-    def dbConnection():
+    def db_connection(self):
         connection = None
-        db_config = load_db_config(config_file_path)
         try:
-    # Verbindung herstellen
             connection = mysql.connector.connect(
-            host=config['host'],
-            user=config['user'],
-            passwd=config['password'],
-            database=config['database']
-        )
+                host=self.config['host'],
+                user=self.config['user'],
+                passwd=self.config['password'],
+                database=self.config['database']
+            )
             if connection.is_connected():
                 print("Erfolgreich verbunden zu MySQL-Datenbank")
-                # Einen Cursor erstellen
                 cursor = connection.cursor()
-                # Eine Abfrage ausführen
                 cursor.execute("SELECT DATABASE();")
                 record = cursor.fetchone()
                 print("Du bist verbunden zu Datenbank:", record)
@@ -32,18 +31,21 @@ class DB:
             print("Fehler beim Verbinden zur MySQL-Datenbank", e)
         return connection
 
-    def insertVideo(path, tags):
-        cursor = connection.cursor()
-        try:
-            cursor.execute("INSERT INTO videos (pfad, tags) VALUES (%s, %s)", (pfad, tags))
-            connection.commit()
-            print("Zeile erfolgreich hinzugefügt")
-        except Error as e:
-            print(f"Fehler beim Einfügen der Zeile: {e}")
-        finally:
-            cursor.close()
+    def insert_video(self, path, tags):
+        connection = self.db_connection()
+        if connection is not None and connection.is_connected():
+            try:
+                with connection.cursor() as cursor:
+                    cursor.execute("INSERT INTO videos (pfad, tags) VALUES (%s, %s)", (path, tags))
+                    connection.commit()
+                    print("Zeile erfolgreich hinzugefügt")
+            except Error as e:
+                print(f"Fehler beim Einfügen der Zeile: {e}")
+            finally:
+                connection.close()
+        else:
+            print("Keine Verbindung zur Datenbank")
 
-#debugging
-if __name__ == "__main__":
-    load_db_config()
-
+# Example usage:
+#db_instance = DB()
+#db_instance.insert_video('/path/to/video.mp4', 'tag1,tag2,tag3')
