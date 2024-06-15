1eec1076ca66e5d1b593516e49434c83579b0174
juliantomsa <julian.tomsa@gmail.com>
2024-06-15 16:10:50 +0200

    use html instead of php

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import aiofiles
 import logging
 from fastapi.staticfiles import StaticFiles
 from concurrent.futures import ProcessPoolExecutor
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
@@ -21,111 +22,19 @@ UPLOAD_DIRECTORY = "uploads"
 # Stelle sicher, dass das Upload-Verzeichnis existiert
 Path(UPLOAD_DIRECTORY).mkdir(parents=True, exist_ok=True)
 
+templates = Jinja2Templates(directory="page")
+
 @app.get("/", response_class=HTMLResponse)
-async def main_page():
+async def main_page(request: Request):
     src_path = os.path.join(os.path.dirname(__file__), 'src')
     sys.path.append(src_path)
     from db import DB
-    
-    content = f"""<!DOCTYPE html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="/page/css/style.css">
-
-    <title>Datei hochladen</title>
-</head>
-<style>
-    ul {{
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: darkolivegreen;
-}}
-
-nav{{
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: darkolivegreen;
-}}
-
-li {{
-    float: left;
-}}
-
-li a {{
-    display: block;
-    color: white;
-    text-align: center ;
-    padding: 14px 16px;
-    text-decoration: none;
-}}
-
-li a:hover:not(.active) {{
-    background-color: white;
-    color: #212121;
-}}
-
-.active {{
-    background-color: #212121;
-}}
-
-.active:hover{{
-    background-color: white;
-    color: #212121;
-}}
-body{{
-    /*background-color: #212121;*/
-    margin: 0;
-    padding: 0;
-}}
-    footer {{
-        position: absolute;
-        background-color: #4caf50;
-        width: 100%;
-    }}
-</style>
-<script>
-    function get_height() {{
-        return window.innerHeight;
-    }}
-
-    function foot() {{
-        return get_height() - 46;
-    }}
-
-    function setFooterPosition() {{
-        var footer = document.getElementById('myFooter');
-        footer.style.marginTop = foot() + 'px';
-    }}
-
-    window.onload = setFooterPosition;
-    window.onresize = setFooterPosition; // Adjust footer position if window is resized
-</script>
-
-<body>
-<footer id="myFooter">
-    <p>halle</p>
-</footer>
-<ul>
-    <li><a href= "/"> Home </a></li>
-    <li><a href= "/upload/"> Upload </a></li>
-</ul>
-
-    <h1>Datei hochladen</h1>
-
-    {DB.all_videos()}
-
-
-
-</body>
-</html>
-"""
-    return HTMLResponse(content=content)
+    try:
+        videos_html = DB.all_videos()
+        return templates.TemplateResponse("index.html", {"request": request, "videos": videos_html})
+        #return HTMLResponse(content=content, status_code=200)
+    except FileNotFoundError:
+        return HTMLResponse(content="File not found", status_code=404)
 
 
 @app.get("/upload/", response_class=HTMLResponse)
