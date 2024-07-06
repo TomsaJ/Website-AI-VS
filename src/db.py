968208d9b1e8f64cadcb7bacb96294ee54deac86
juliantomsa <julian.tomsa@gmail.com>
2024-07-06 13:14:12 +0200

    chnage inser_video and change video for one user or all videos

--- src/db.py
+++ src/db.py
@@ -18,42 +18,62 @@ class DB:
         except Error as e:
             print(f"Fehler bei der Datenbankverbindung: {e}")
 
-    def insert_video(path, tags):
-        print("path: "+ path + "tags: " + tags)
+    def insert_video(path, user):
         connection = DB.db_conn()
         try:
             with connection.cursor() as cursor:
                 # Convert tags list to JSON string
                 #                     tags_json = json.dumps(tags)
-                cursor.execute("INSERT INTO videos (pfad, tags) VALUES (%s, %s)", (path, tags))
+                cursor.execute("INSERT INTO videos (pfad, user) VALUES (%s, %s)", (path, user))
                 connection.commit()
-                print("Zeile erfolgreich hinzugefügt")
         except Error as e:
             print(f"Fehler beim Einfügen der Zeile: {e}")
         finally:
             connection.close()
 
-    def all_videos():
+    def videos(user):
         connection = DB.db_conn()
-        try:
-            with connection.cursor() as cursor:
-                cursor.execute("SELECT pfad FROM videos")
-                myresult = cursor.fetchall()
+        if user == "null":
+            try:
+                with connection.cursor() as cursor:
+                    cursor.execute("SELECT pfad FROM videos")
+                    myresult = cursor.fetchall()
             # Generate HTML video elements for each path
-                video_elements = ''.join([
+                    video_elements = ''.join([
+                    f'<video width="320" height="270" controls>'
+                f'<source src="{x[0]}" type="video/mp4">'
+                'Your browser does not support the video tag.'
+                '</video>'
+                for x in myresult
+                ])
+                return video_elements
+            except Error as e:
+                print(f"Fehler beim Abrufen der Daten: {e}")
+                return "<p>Empty</p>"
+            finally:
+                if connection.is_connected():
+                    connection.close()
+        else:
+            try:
+                with connection.cursor() as cursor:
+                    sql_query = "SELECT pfad FROM videos where user = %s"
+                    cursor.execute(sql_query, (user,))
+                    myresult = cursor.fetchall()
+                # Generate HTML video elements for each path
+                    video_elements = ''.join([
                 f'<video width="320" height="270" controls>'
                 f'<source src="{x[0]}" type="video/mp4">'
                 'Your browser does not support the video tag.'
                 '</video>'
                 for x in myresult
                 ])
-            return video_elements
-        except Error as e:
-            print(f"Fehler beim Abrufen der Daten: {e}")
-            return "<p>Empty</p>"
-        finally:
-            if connection.is_connected():
-                connection.close()
+                return video_elements
+            except Error as e:
+                print(f"Fehler beim Abrufen der Daten: {e}")
+                return "<p>Empty</p>"
+            finally:
+                if connection.is_connected():
+                    connection.close()
     
     def all_lang():
         connection = DB.db_conn()
@@ -95,7 +115,39 @@ class DB:
             if connection.is_connected():
                 connection.close()  # Schließen Sie die Verbindung zur Datenbank, wenn sie geöffnet ist
 
-        #language_code
+    def login(username, password):
+        connection = DB.db_conn()
+        if connection is None:
+            return None  # Rückgabe None, um anzuzeigen, dass keine Verbindung hergestellt werden konnte
+        try:
+            with connection.cursor() as cursor:
+                sql_query = "SELECT password FROM user WHERE username = %s"
+                cursor.execute(sql_query, (username,))
+                myresult = cursor.fetchone()  # Da wir nur einen Wert erwarten, verwenden wir fetchone()
+                if myresult:
+                    if password == myresult[0]:
+                        return True
+                    else:
+                        return False   # Rückgabe des ersten Elements des Tupels (language_code)
+            cursor = connection.cursor()
+            sql_query = "SELECT password FROM user WHERE username = %s"
+            cursor.execute(sql_query, (username,))
+            myresult = cursor.fetchone()
+            if myresult:
+                stored_password = myresult[0]
+                if password == stored_password:
+                    return True
+                else:
+                    return False
+            else:
+                return None  # Rückgabe None, wenn kein Ergebnis gefunden wurde
+        except Error as e:
+            print(f"Error fetching data: {e}")
+            return None  # Rückgabe None im Falle eines Fehlers
+        finally:
+            cursor.close()
+            if connection.is_connected():
+                connection.close()  # Schließen Sie die Verbindung zur Datenbank, wenn sie geöffnet ist
 
 # Example usage
 # DB.insert_video("/path/to/video.mp4", ["tag1", "tag2", "tag3"])
