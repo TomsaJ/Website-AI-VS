78480bc83fa0477db18a19740cc316c4e28fa859
TomsaJ <julian.tomsa@gmail.com>
2024-08-10 14:59:57 +0000

    -

--- test/model_comparison.py
+++ test/model_comparison.py
@@ -12,26 +12,24 @@ PATH_SEPARATOR = '\\' if os.name == 'nt' else '/'
 
 def main():
     a = ['tiny', 'base', 'small', 'medium', 'large']
+    v = ['1min.mp4','10min.mp4','15min.mp4', '20min.mp4', '30min.mp4', '45min.mp4', '90min.mp4']
+    print("Current Model;Video; Sekunden; Minuten; ")
     for model in a:
-        tmp_time = time.time()
-        file_location = '10min.mp4'
-        file_path = Path(file_location)
-        file_name = file_path.name
-        filename = FileManager.get_file_name(file_path)
-        tmp_file_path = FileManager.copy_to_tmp_directory(file_path, filename)
-        FileManager.delete_tmp_file(file_path)
-        print(tmp_file_path)
-        SubtitleGen.create_subtitles(tmp_file_path, filename, 'German', model)
-        output_file = 'test/videos' + model+ PATH_SEPARATOR + filename + '_'+ time.time() + '_subtitle.mp4'
-        subtitle = 'test/videos' + model + PATH_SEPARATOR + filename + '_'+ time.time() +  '_subtitle.srt'
-        file_path = 'test/videos' + model + PATH_SEPARATOR + filename + '_'+ time.time() +  '.mp4'
-        lang = Db.get_language_code(lang)
-        FileManager.combine_video_with_subtitle(file_path, subtitle, output_file, 'ger')
-        current_time = time.time()
-        print("Untertitelerstellungsdauer:")
-        print ((current_time - tmp)/1000 + " Sekunden")
-        print ((current_time - tmp)/6000 + " Minuten")
-    print("Fertig")
+        for video in v:
+            
+            tmp_time = time.time()
+            file_location = video
+            file_path = Path(file_location)
+            file_name = file_path.name
+            filename = FileManager.get_file_name(file_path)
+            tmp_file_path = FileManager.copy_to_tmp_directory(file_path, filename)
+            SubtitleGen.create_subtitles(tmp_file_path, filename, 'German', model)
+            output_file = 'test/videos' + PATH_SEPARATOR + filename + '_subtitle.mp4'
+            subtitle = 'test/videos'  + PATH_SEPARATOR + filename  +  '_subtitle.srt'
+            file_path = 'test/videos'  + PATH_SEPARATOR + filename  +  '.mp4'
+            FileManager.combine_video_with_subtitle(file_path, subtitle, output_file, 'ger')
+            current_time = time.time()
+            print (model + ";"+ video + ";" + str((current_time - tmp_time)) + ";" + str((current_time - tmp_time)/60)+";")
 
 
 if __name__ == "__main__":
