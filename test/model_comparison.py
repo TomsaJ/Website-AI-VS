6ae527adc89db1f41810e71069147c7a07e924e2
juliantomsa <julian.tomsa@gmail.com>
2024-08-14 20:34:05 +0200

    more videos

--- test/model_comparison.py
+++ test/model_comparison.py
@@ -21,28 +21,26 @@ if _create_unverified_https_context is not None:
     # Setze den unsicheren SSL-Kontext als Standard, wenn verfügbar
     ssl._create_default_https_context = _create_unverified_https_context
 
-# Beispiel für eine HTTP-Anfrage
-#response = urllib.request.urlopen('https://example.com')
-#print(response.read())
 
 
 PATH_SEPARATOR = '\\' if os.name == 'nt' else '/'
 
 def main():
     a = ['tiny', 'base', 'small', 'medium', 'large']
-    v = ['1min.mp4','10min.mp4','15min.mp4', '20min.mp4', '30min.mp4', '45min.mp4']
-    print("Current Model;Video; Sekunden; Minuten; ")
+    v = ['1min.mp4','5min.mp4','10min.mp4','15min.mp4', '20min.mp4','25min.mp4', '30min.mp4','35min.mp4','40min.mp4', '45min.mp4']
+    print("Current Model;Video; Video in Sekunden; Sekunden; Minuten")
     for model in a:
         for video in v:
             
             tmp_time = time.time()
-            file_location = video
+            file_location = "test_video/" + video
+            video_duration = FileManager.duration_video(file_location)
             file_path = Path(file_location)
             filename = FileManager.get_file_name(file_path)
             tmp_file_path = FileManager.copy_to_tmp_directory(file_path, filename)
             SubtitleGen.create_subtitles(tmp_file_path, filename, 'German', model)
             current_time = time.time()
-            print (model + ";"+ video + ";" + str((current_time - tmp_time)) + ";" + str((current_time - tmp_time)/60)+";")
+            print (model + ";" + video + ";" + str(video_duration) + ";" + str((current_time - tmp_time)) + ";" + str((current_time - tmp_time)/60))
 
 
 if __name__ == "__main__":
