c9c07877b9faae1bf1988f59ee4cb6f92a929735
juliantomsa <julian.tomsa@gmail.com>
2024-08-16 15:54:28 +0200

    add test

--- UserTest.py
+++ UserTest.py
@@ -32,12 +32,25 @@ class TestUserAuthentication(unittest.TestCase):
         password = User.hash_password(password, salt)
         password = salt.decode('utf-8') + password.decode('utf-8')
         data = password, "admin"
-        mock = "admin", password
-        mock_login_user.return_value = mock
+        mock_login_user.return_value = data
         # Test user authentication
         result = User.authenticate_user(username, password)
-        print (result)
         self.assertEqual(result, "admin")
+        mock_login_user.assert_called_once_with(username)
+
+    @patch('src.db.Db.login_user')
+    def test_authenticate_user_failed(self, mock_login_user):
+        username = "admin"
+        password = "admin"
+        salt = User.generate_salt()
+        password = User.hash_password(password, salt)
+        password = salt.decode('utf-8') + password.decode('utf-8')
+        data = password, "admin"
+        mock_login_user.return_value = data
+        # Test user authentication
+        result = User.authenticate_user(username, "password")
+        print (result)
+        self.assertEqual(result, False)
         mock_login_user.assert_called_once_with(username)
 
 if __name__ == '__main__':
